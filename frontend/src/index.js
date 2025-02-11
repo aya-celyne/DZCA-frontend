2280edd0da26b60bea733568f189cbe687a97d6c
Aya Celine <59054943+aya-celyne@users.noreply.github.com>
2025-02-10 21:44:13 -0600

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,19 +19,5 @@ root.render(
 );
 
 
-const path = require('path');
-const express = require('express');
-const app = express();
 
-// Serve static files from React's build folder
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// Handle ALL frontend routes by serving React's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
